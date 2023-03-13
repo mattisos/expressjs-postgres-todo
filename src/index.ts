0affe407e561db56aa3333c7a595b38962c89687
mattisos <mattis.osula@funkdigital.ee>
2023-03-13 13:51:03 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,24 @@ import pg from "pg";
 
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
-// const pool = new pg.Pool();
+const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(express.json());
 
 app.get("/", async (req, res) => {
-// const { rows } = await pool.query("SELECT NOW()");
-  res.send(JSON.stringify({ text: "todo1" }));
+  const { rows } = await pool.query("SELECT * FROM todos;");
+  console.log(rows)
+  res.send(JSON.stringify(rows));
+});
+
+app.post("/", async (req, res) => {
+  await pool.query(`INSERT INTO todos (text) VALUES "${req.body.text}";`);
+  const { rows } = await pool.query("SELECT * FROM todos;");
+  res.send(JSON.stringify(rows));
 });
 
 app.listen(port, () => {
